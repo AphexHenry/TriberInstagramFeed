1571a861ddc3a94108652ff935de752b5cea6c39
AphexHenry <baptiste.bohelay@gmail.com>
2015-03-11 12:04:04 +0100

    Add videos feature

--- public/js/feed.js
+++ public/js/feed.js
@@ -7,54 +7,63 @@ var socket = io();
 /*
  *  Angular.
  */
-angular.module("mainModule", [])
-  .controller("FeedController", function ($scope, $http)
+var angularApp = angular.module("mainModule", []);
+
+angularApp.config(function($sceDelegateProvider) {
+     $sceDelegateProvider.resourceUrlWhitelist([
+       // Allow same origin resource loads.
+       'self',
+       // Allow videos loading from the instagram domain.
+       'http://scontent.cdninstagram.com/**']);
+     });
+
+angularApp.controller("FeedController", function ($scope, $http)
+{
+  // container for the posts.
+  $scope.posts = [];
+  //loading state.
+  $scope.loading = true;
+
+  var lId = sURLTools.getUrlParameter("userid");
+  // sent the id to the server.
+  socket.emit('setId', lId);
+  //retrieve the user information.
+  socket.on('mapUser', function(msg)
   {
-    // container for the posts.
-    $scope.posts = [];
-    //loading state.
-    $scope.loading = true;
+    $scope.user = msg;
+    // request the feed.
+    AskForNextPosts($scope);
 
-    var lId = sURLTools.getUrlParameter("userid");
-    // sent the id to the server.
-    socket.emit('setId', lId);
-    //retrieve the user information.
-    socket.on('mapUser', function(msg)
-    {
-      $scope.user = msg;
-      // request the feed.
-      AskForNextPosts($scope);
-
-      // get the next posts.
-      socket.on('addFeed', function(msg){
-        $scope.loading = false;
-        if(msg.index == 0)
-        {
-          $scope.posts = msg.content.concat($scope.posts);
-        }
-        else
-        {
-          $scope.posts = $scope.posts.concat(msg.content);  
-        }
-        
-        $(window).scroll(function() {
-           if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-            if(!$scope.loading)
-            {
-              AskForNextPosts($scope);
-              $scope.loading = true;
-            }
-           }
-        });
-
-        // $scope.posts = [{user:{username:"fdsdf"}}, {user:{username:"dsf"}}];
-        $scope.$apply();
+    // get the next posts.
+    socket.on('addFeed', function(msg){
+      $scope.loading = false;
+      if(msg.index == 0)
+      {
+        $scope.posts = msg.content.concat($scope.posts);
+      }
+      else
+      {
+        $scope.posts = $scope.posts.concat(msg.content);  
+      }
+      
+      $(window).scroll(function() {
+         if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+          if(!$scope.loading)
+          {
+            AskForNextPosts($scope);
+            $scope.loading = true;
+          }
+         }
       });
+
+      // $scope.posts = [{user:{username:"fdsdf"}}, {user:{username:"dsf"}}];
+      $scope.$apply();
     });
   });
+});
 
-  function AskForNextPosts($scope)
-  {
-    $scope.loading = true;
-    socket.emit('getFeed', $scope.posts.length);
-  }
+function AskForNextPosts($scope)
+{
+  $scope.loading = true;
+  socket.emit('getFeed', $scope.posts.length);
+}
